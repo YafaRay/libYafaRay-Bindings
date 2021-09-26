7936830eb7ac4f704cab11ccf655baa00d4fb9d1
DavidBluecame <david.bluecame@gmail.com>
2021-09-26 12:28:13 +0200

    Fixed crash when using paramsSetMatrix

--- src/bindings_python/libyafaray_python.cc
+++ src/bindings_python/libyafaray_python.cc
@@ -291,10 +291,10 @@ PyObject *paramsSetMatrix(YafaRayInterface *self, PyObject *args)
 	float matrix[4][4];
 	int transpose = YAFARAY_BOOL_FALSE;
 	if(!PyArg_ParseTuple(args, "sffffffffffffffff|p", &str,
-						 matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3],
-						 matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3],
-						 matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3],
-						 matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3],
+						 &matrix[0][0], &matrix[0][1], &matrix[0][2], &matrix[0][3],
+						 &matrix[1][0], &matrix[1][1], &matrix[1][2], &matrix[1][3],
+						 &matrix[2][0], &matrix[2][1], &matrix[2][2], &matrix[2][3],
+						 &matrix[3][0], &matrix[3][1], &matrix[3][2], &matrix[3][3],
 						 &transpose
 	)) Py_RETURN_FALSE;
 	yafaray_paramsSetMatrix(self->interf_, str, reinterpret_cast<const float *>(matrix), static_cast<yafaray_bool_t>(transpose));
